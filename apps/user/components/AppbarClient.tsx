b4b178fbecbc4a7e72981da2fc62e22a38d709c6
Anuj Dubey <anujd2408@gmail.com>
2025-06-21 00:38:10 +0530

    updated many things

--- apps/user/components/AppbarClient.tsx
+++ apps/user/components/AppbarClient.tsx
@@ -8,11 +8,14 @@ export function AppbarClient() {
   const router = useRouter();
 
   return (
-   <div>
-      <Appbar onSignin={signIn} onSignout={async () => {
-        await signOut()
-        router.push("/api/auth/signin")
-      }} user={session.data?.user} />
-   </div>
+    <div>
+      <Appbar
+        onSignin={signIn}
+        onSignout={async () => {
+          await signOut({ callbackUrl: "/" }); // ✅ Redirects to landing page
+        }}
+        user={session.data?.user}
+      />
+    </div>
   );
 }

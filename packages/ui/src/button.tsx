7708f64b89e95c75806d9f8260eb2076f0d689b1
Anuj Dubey <anujd2408@gmail.com>
2025-06-22 01:00:09 +0530

    updated button

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -17,12 +17,12 @@ export const Button = ({
   ...rest
 }: ButtonProps) => {
   const baseClasses =
-    "transition font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2";
+    "transition font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 cursor-pointer";
   const disabledClasses = disabled ? "opacity-50 cursor-not-allowed" : "";
   
   const variantClasses =
     variant === "inverted"
-      ? "bg-white text-blue-600 border border-white hover:bg-transparent hover:text-white"
+      ? "bg-white text-blue-600 border border-white hover:bg-transparent hover:text-white cursor-pointer"
       : "bg-blue-600 text-white hover:bg-blue-700";
 
   return (

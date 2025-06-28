e60dc8d82dcae15f9d8ed0786d3d9ad48243bc40
Anuj Dubey <anujd2408@gmail.com>
2025-06-28 19:23:25 +0530

    build fixed

--- apps/user/app/api/user/route.ts
+++ apps/user/app/api/user/route.ts
@@ -1,20 +1,24 @@
-import { getServerSession } from "next-auth"
+import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
-import { authOptions } from "../../lib/auth";
+import { authOptions } from "../../lib/auth"; // Adjust the import path as needed
 
 export const GET = async () => {
-    try{
-        const session = await getServerSession(authOptions);
-        if (session.user) {
-            return NextResponse.json({
-            user: session.user
-        })
+  try {
+    const session = await getServerSession(authOptions);
+    if (session && session.user) {
+      return NextResponse.json({
+        user: session.user,
+      });
     }
-    }catch(e){
-        return NextResponse.json({
-            message: "You are not logged in"
-        }, {
-            status: 403
-        })
-    }
-}
+
+    return NextResponse.json(
+      { message: "You are not logged in" },
+      { status: 403 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Something went wrong" },
+      { status: 500 }
+    );
+  }
+};
